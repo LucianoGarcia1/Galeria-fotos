6d9a82f358cb4ace374727daa9c784389249f26f
LucianoGarcia1 <lucianogarcia0210@gmail.com>
2023-05-24 21:08:27 -0300

    up galeria

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import NotFoundImg from "../assets/img3.svg"
 export const NotFound = () => {
   return (
     <section className="container pageNot">
-      <h1>Error: 404 - <span>Page Not Found</span></h1>
+      <h1>Error: 404 - <span>Pagina não existe</span></h1>
 
       <img src={NotFoundImg} alt="Not Found" />
     </section>
